9f7804078d95976a3ca5c9bb0eb4aafe0e1ebb6f
Gormi3815 <gormi3815@wrdsb.ca>
2020-09-14 15:47:37 +0000

    Made a program thatcalculates how much it costs to buy certain chromebook parts

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  *
  * @author 
@@ -10,6 +12,21 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println ("How many chromebook chargers would you like");
+    int Chromebook_chargers =input.nextInt();
+    double Chromebook_chargerstotal  =Chromebook_chargers  * 34.99;
+    System.out.println("How many motherboards would you like");
+    int Motherboard =input.nextInt();
+    double Motherboardtotal =Motherboard * 127.50;
+    System.out.println("How many mice would you like");
+    int Mice =input.nextInt();
+    int Micetotal =Mice*18;
+    double Subtotal = Chromebook_chargerstotal+Motherboardtotal+Micetotal;
+    System.out.println("Your Subtotal is " + Subtotal + "." );
+double taxes = Subtotal*0.13;
+System.out.println("Tax is " + taxes + ".");
+    double total = Subtotal+taxes;
+    System.out.println("Your total is " + total + ".");
   }
 }
